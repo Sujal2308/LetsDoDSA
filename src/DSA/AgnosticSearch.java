2348c39e08306363d916c4366d42310f902bf411
Sujal2308 <sujalbhugul08@gmail.com>
2024-12-18 22:34:10 +0530

    Agnostic Searching

---
+++ src/DSA/AgnosticSearch.java
@@ -0,0 +1,43 @@
+package DSA;
+
+public class AgnosticSearch {
+    public static void main(String[] args) {
+        int[] arr = {100,23,11,2,-10};
+        int target = 2;
+        System.out.println(BS3(arr, target));
+    }
+
+    static int BS3(int[] sortArr , int target){
+        int start = 0;
+        int end = sortArr.length-1;
+        boolean isAscending = sortArr[start] < sortArr[end];
+        while(start<=end){
+            int middle = start + (end-start)/2;
+            if(isAscending){
+                // sorted in ascending fashion
+                if(target>sortArr[middle]){
+                    // It means target lies in right side of middle
+                    start = middle + 1;
+                } else if (target < sortArr[middle]) {
+                    // It means target lies in left side of middle
+                    end = middle - 1;
+                }else return middle;
+            }else{ // sorted in descending fashion
+                if(target > sortArr[middle]){
+                    // It means target lies in left side of middle
+                    end = middle-1;
+                } else if (target < sortArr[middle]) {
+                    // target lies in right side of middle
+                    start = middle + 1;
+
+                }else return middle; // target == sortedArr[middle]
+            }
+        }
+        return -1;
+    }
+
+
+}
+
+
+
