3bbf37e2de099dcce44839b4a645edc26e383081
Sujal2308 <sujalbhugul08@gmail.com>
2025-01-02 23:22:21 +0530

    printing alphabets

---
+++ src/DSA/Strings/PrintingAlphabets.java
@@ -0,0 +1,10 @@
+package DSA.Strings;
+
+public class PrintingAlphabets {
+    public static void main(String[] args) {
+        for(char ch = 'a'; ch<='z' ;ch++){
+            // characters behind the scenes are treated as integer values
+            System.out.print(ch);
+        }
+    }
+}
