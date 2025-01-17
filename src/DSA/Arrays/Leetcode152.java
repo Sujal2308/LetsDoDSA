07c46fe38891bbd52610d253783b1be57726b067
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-01-18 01:44:21 +0530

    Find Single Number -2

---
+++ src/DSA/Arrays/Leetcode152.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class LeetCode136 {
+    public static void main(String[] args) {
+    int[] nums = {4,1,2,1,2};
+        System.out.println(singleNumber(nums));
+    }
+    static int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+
+        for(int i = 0; i<nums.length; i=i+3){
+            if(i==nums.length-1){
+                return nums[nums.length-1];
+            }
+                if(nums[i]!=nums[i+1] && i<nums.length-1){
+                 return nums[i];
+                }
+
+            }
+        return nums[nums.length-1];
+
+    }
+}
