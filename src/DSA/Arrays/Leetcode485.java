08afd60d01ca7b48f0658f79b2bd16f8178ec79e
Sujal Bhugul <133880090+Sujal2308@users.noreply.github.com>
2025-02-13 02:34:06 +0530

    Max Consecutive Ones

---
+++ src/DSA/Arrays/Leetcode485.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count1 = 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count1++;
+            } else {
+                if (count1 > max) {
+                    max = count1;
+                }
+
+                count1 = 0;
+            }
+        }
+        if (count1 > max) {
+            max = count1;
+        }
+        return max;
+    }
+}
